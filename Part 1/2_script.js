69789dc53c9636ed1eec1d936e40bcba5d29d4d4
Roshni <roshnisahu11222@gmail.com>
2026-07-18 00:45:57 +0530

    Add JS Concepts

---
+++ Part 1/2_script.js
@@ -0,0 +1,33 @@
+/*var
+char
+ and let */
+
+// var
+var fullName="Rani";
+
+var fullName="pinku";
+
+var fullName="Roshni";
+console.log(fullName);
+
+// let
+let data=10;
+data=20;
+data=30;
+console.log(data);
+
+// const
+const PI=3.14;
+// PI=3.14;
+console.log(PI);
+
+// Separate Block
+{
+    a=5;
+    console.log(a);
+}
+{
+    a=10;
+    console.log(a);
+    
+}
