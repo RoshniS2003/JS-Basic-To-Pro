f3efbef953e87789dbf43dac9fd32ebbc753cf01
Roshni <roshnisahu11222@gmail.com>
2026-07-18 00:43:34 +0530

    Add JS Concepts

---
+++ Part 1/1_script.js
@@ -0,0 +1,29 @@
+console.log('Good Morning!..');
+console.log('Hello, I am Roshni Sahu');
+
+// Variables are container for data
+// variables = Value;
+name='Roshni';
+console.log(name);
+
+// null means no value present...its empty
+a=null;
+b=undefined;
+console.log(a);
+console.log(b);
+// false
+isfollow=true;
+console.log(isfollow);
+
+// Dynamically Typed
+data = 10;
+console.log(data);  // "Number"
+
+data = "Roshni";
+console.log(data);  // "String"
+
+data = 10;
+console.log(typeof data);  // "Number"
+
+data = "Roshni";
+console.log(typeof data);  // "String"
