19ad7372d3ffaf773bb1bd526f033864f0583d91
Roshni <roshnisahu11222@gmail.com>
2026-07-20 22:53:16 +0530

    Added Script file

---
+++ Part 1/3_script.js
@@ -0,0 +1,24 @@
+// Number
+let a=10;
+let b=10.55;
+console.log(typeof a);
+console.log(typeof b);
+
+// String
+let name="roshni";
+console.log(typeof name);
+
+// Boolean
+let isfollow=true;
+console.log(typeof isfollow);
+
+//undefined
+let x;
+console.log(x);
+console.log(typeof x);
+
+//null
+let y=null;
+console.log(y);
+console.log(typeof y);
+
