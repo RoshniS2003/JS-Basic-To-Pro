7e3a4342a75a2fae7ae6f6dc210fbc4ff67e79f7
Roshni <roshnisahu11222@gmail.com>
2026-07-22 15:47:08 +0530

    Added Script File

---
+++ Part 1/4_script.js
@@ -0,0 +1,17 @@
+// char also use in place of let
+let student = {
+    name:"roshni",
+    age:23,
+    preMca:417
+};
+
+// value Updates
+student["name"] = "Miss Roshni Sahu";
+student["age"] = student["age"] + 1 ;
+
+console.log(student);
+console.log(typeof student);
+console.log(student.name);
+console.log(student["age"]);
+
+
