6a8f0e0b80052a6bafd47b05dc5325d0d3b6f7d2
Roshni <roshnisahu11222@gmail.com>
2026-07-23 10:55:38 +0530

    Add Script File

---
+++ Part 1/5_script.js
@@ -0,0 +1,24 @@
+const penProduct = {
+    title : "Parker Jotter",
+    rating : "5001" ,
+    price : 450,
+    offer : "5% off",
+    isBuy: true
+};
+
+console.log(penProduct);
+console.log(typeof penProduct);
+console.log(typeof penProduct["title"]);
+console.log(typeof penProduct.isBuy);
+
+let a = "abc";
+a = a + 123;
+console.log(a);
+
+
+let b = "123" + 123;
+console.log(b);
+
+let c = 12 + 13;
+console.log(c);
+
