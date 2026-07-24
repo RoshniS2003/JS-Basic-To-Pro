d42d01d5d97e191f7e785d8eb2c23d7cf1e5ff90
Roshni <roshnisahu11222@gmail.com>
2026-07-24 23:04:56 +0530

    Added Script Files

---
+++ Part 2/1_script.js
@@ -0,0 +1,25 @@
+// Arithmetic operator
+let a=3;
+let b=2;
+let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=a/b;
+let g=a%b;
+// exponentiation
+let h=a ** b;
+
+console.log("a=" ,a, "b=",b);
+console.log("Add:" ,c);
+console.log("Sub:" ,d);
+console.log("multi:",e);
+console.log("div:",f);
+console.log("remainder:",g);
+console.log("expon:",h);  //a^b
+
+
+
+
+
+
+
