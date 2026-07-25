f8b529dc80a84cf66bdcec70b9452c37a2d0bbf3
Roshni <roshnisahu11222@gmail.com>
2026-07-25 12:16:20 +0530

    Added Script File

---
+++ Part 2/2_script.js
@@ -0,0 +1,20 @@
+//Unary Operator
+// a++ (a=a+1)--->>Increment
+// a-- (a=a-1)---->> Decrement
+let p= 5;
+let q=10;
+
+console.log("p=",p,"q=",q);
+/*p++;
+q=q+1;
+console.log("p=",p);
+console.log("q=",q);*/
+
+// p--;
+// q=q-1;
+// console.log("p=",p); 
+// console.log("q=",q); 
+
+// console.log("++p =",++p);
+console.log("p++ =",p++);
+console.log("p=",p);
