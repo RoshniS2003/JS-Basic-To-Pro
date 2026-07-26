c8b5615f6071fea93e0a2c8ccd1837a5bb0fad79
Roshni <roshnisahu11222@gmail.com>
2026-07-26 16:31:46 +0530

    Added Script File

---
+++ Part 2/3_script.js
@@ -0,0 +1,24 @@
+// Assignment Operator
+// = , += , -= , *= , %= , **=
+let a = 5;
+let b = 4;
+
+a -= 5; // a=a+5 // 10
+console.log("a=", a);
+
+// Comparison Operator
+//  == , != , === , !== , > , < , >= , <=
+// === check datatype and values strictly
+let p=5;
+let q=5;
+console.log("p == q",p == q);
+console.log("p === q",p === q);
+console.log("p != q",p != q);
+
+let r=5; //number
+let s="5";  // string convert to number
+console.log("r == s",r == s);
+console.log("r === s",r === s);
+console.log("r !== s",r !== s);
+
+
