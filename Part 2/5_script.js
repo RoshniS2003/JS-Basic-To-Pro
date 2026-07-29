7531d11b3d05104a4233a58ecbfe64ddd6934317
Roshni <roshnisahu11222@gmail.com>
2026-07-29 17:10:42 +0530

    Added Script File

---
+++ Part 2/5_script.js
@@ -0,0 +1,58 @@
+// Age Example
+/* let age = 25;
+
+if (age >= 18) {
+    console.log("You can vote.");
+}     */
+
+let age = 25;
+
+if (age >= 18) {
+    console.log("You can vote.");
+} else {
+    console.log("you cannot vote.");
+
+}
+
+
+// dark and light mode
+/* let mode = "dark"
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if (mode === "light") {
+    color = "white";
+} 
+
+console.log(color); */
+
+
+// if - else statement
+// dark and light mode
+let mode = "dark"   // red
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+else {
+    color = "white";
+}
+
+console.log(color);
+
+
+
+// Even & odd example
+let num=10;
+
+if (num % 2 === 0) {
+    console.log(num, "is a even");
+} else {
+    console.log(num, "is a odd");
+}
+
+
