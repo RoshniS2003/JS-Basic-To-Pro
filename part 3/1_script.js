79585f69970b30c473ac031d61996e977e9b6cdd
Roshni <roshnisahu11222@gmail.com>
2026-07-31 23:43:11 +0530

    Added Script File

---
+++ part 3/1_script.js
@@ -0,0 +1,27 @@
+// Initialization , stopping condition , Incre/decre (updation)
+for(let i=1 ; i <= 5 ; i++){
+    // console.log("Roshni Sahu");
+    console.log("i =",i);
+}
+console.log("Loop has ended.");
+
+
+for(var i=1 ; i <= 5 ; i++){
+    console.log("i =",i);
+}
+console.log(i);
+
+console.log("Loop has ended.");
+
+
+// calculate sum of 1 to 5
+let sum=0;
+// let n= 6
+// for (let j=1 ; j<=n ; j++){
+for (let j=1 ; j<=5 ; j++){
+    sum = sum + j;
+}
+console.log("sum =",sum);   // 15
+console.log("loop has ended.");
+
+
