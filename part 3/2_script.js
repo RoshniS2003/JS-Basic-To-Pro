49b272826ead57bda96dd17a7ed9de27154872e3
Roshni <roshnisahu11222@gmail.com>
2026-08-01 22:50:43 +0530

    Added Script File

---
+++ part 3/2_script.js
@@ -0,0 +1,30 @@
+// Infinite loop -->> A loop that never ends
+ /* for (let i=1 ; i >= 0 ; i++){
+    console.log("i=",i);
+    
+}  */
+
+// while loop
+let r=1;
+while (r <= 5) {
+    console.log("r =",r); 
+    r++;  
+}
+console.log("loop has ended.");
+
+
+// do - while loop
+let s=1;
+do {
+    console.log("Rani");
+    s++;
+} while (s <= 5);
+
+
+// first print then condition check
+let j=20;
+do {
+    console.log("Roshni Sahu");
+    j++;
+    
+} while (j <= 10);
