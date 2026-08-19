3b4c5177f02f554423753c3f9add828d90f1ecf6
Roshni <roshnisahu11222@gmail.com>
2026-08-19 23:47:23 +0530

    Added Script File

---
+++ part 5/3_script.js
@@ -0,0 +1,37 @@
+// Create a function using the "function" keyword that takes a string as an argument & returns the number of vowels in the string
+
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if (
+            char === "a" || 
+            char === "e" ||
+            char === "i" ||
+            char === "o" ||
+            char === "u"
+        ) {
+            count++;
+        }
+    }
+    // console.log(count);  //countVowels("Helloworld");
+    return count;
+}
+
+
+// create an arrow function to perform the same task
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (
+            char === "a" || 
+            char === "e" ||
+            char === "i" ||
+            char === "o" ||
+            char === "u"
+        ) {
+            count++;
+        }
+    }
+    // console.log(count);  //countVowels("Helloworld");
+    return count;
+}
